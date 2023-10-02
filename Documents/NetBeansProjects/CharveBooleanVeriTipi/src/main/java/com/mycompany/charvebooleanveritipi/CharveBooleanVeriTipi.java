ac8b73d9d402e96b28611426a05b319ebf2f9c52
ahmetkiyran <ahmet.kiyran1@gmail.com>
2023-10-02 21:39:21 +0300

    java

---
+++ Documents/NetBeansProjects/CharveBooleanVeriTipi/src/main/java/com/mycompany/charvebooleanveritipi/CharveBooleanVeriTipi.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.charvebooleanveritipi;
+
+/**
+ *
+ * @author Ahmet
+ */
+public class CharveBooleanVeriTipi {
+
+    public static void main(String[] args) {
+       /*char veri tipi karakterleri göstermek için kullanılır
+       2 byte yer kaplar 2^16 tane karakter simgelenebilir. */
+       /*boolean  veri tipi koşul durumlarında kullanılır true veya false değerler alır*/
+       
+       char a = 'A';
+       char b = '?';
+       char c = 150;
+       char d = '\u00D3';
+       
+       System.out.println(d);
+       
+       boolean x = true;
+       boolean y = false;
+       
+        System.out.println(x);
+        System.out.println(y);
+        
+        
+    }
+}
