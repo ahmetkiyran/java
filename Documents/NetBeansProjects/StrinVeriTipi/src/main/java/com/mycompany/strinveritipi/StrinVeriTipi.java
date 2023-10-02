f15bba14971ac8d96bddddb6a9294d587dd5d5a6
ahmetkiyran <ahmet.kiyran1@gmail.com>
2023-10-02 22:27:53 +0300

    java

---
+++ Documents/NetBeansProjects/StrinVeriTipi/src/main/java/com/mycompany/strinveritipi/StrinVeriTipi.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.strinveritipi;
+
+/**
+ *
+ * @author Ahmet
+ */
+public class StrinVeriTipi {
+
+    public static void main(String[] args) {
+      
+        /*String a = "merhaba";
+        System.out.println(a +" hoşgeldiniz");*/
+        
+        /*String a = "Java ";
+        String b = "programlama ";
+        String c = "dili";
+        
+        String toplam = a + b + c;
+        
+        System.out.println("toplam "+ toplam);*/
+        
+        /*String a = "merhaba ";
+        int b = 35;
+        
+        String c = a + b;
+        
+        System.out.println(c);*/
+        
+        
+        /*String a = "merhaba ";
+        double b = 3.52;
+        byte c = 10;
+        
+        a = a + b + c;
+        
+        System.out.println(a);*/ // string olduğu için yanyana ekledi toplama yapmadı.
+        
+        /*char a = '?';
+        
+        String b = "merhaba nasılsın " + a;
+        
+        System.out.println(b);*/
+        
+        String a = "java "+
+                "programlama "+ 
+                "dili";
+        
+        System.out.println(a);
+        
+        // \n alt satıra geçmeye yarar. \t 1 tab boşluk bırakır
+    }
+}
