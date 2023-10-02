a870ed81df8c6f2d870b10d5feae2c90f848265a
ahmetkiyran <ahmet.kiyran1@gmail.com>
2023-10-02 18:12:08 +0300

    java

---
+++ Documents/NetBeansProjects/degiskenler1/src/main/java/com/mycompany/degiskenler1/Degiskenler1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.degiskenler1;
+
+/**
+ *
+ * @author Ahmet
+ */
+public class Degiskenler1 {
+
+    public static void main(String[] args) {
+         int a =5;
+        int b =3;
+        int c =9;
+        int toplam;
+        toplam=a*b+c;
+        System.out.println("toplam = " + toplam);
+    }
+}
