d0dd7bea7cacf9fc2034977c1cbc499d8d3830e5
ahmetkiyran <ahmet.kiyran1@gmail.com>
2023-10-02 21:25:47 +0300

    java

---
+++ Documents/NetBeansProjects/TamsayiVeritipleri/src/main/java/com/mycompany/tamsayiveritipleri/TamsayiVeritipleri.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tamsayiveritipleri;
+
+/**
+ *
+ * @author Ahmet
+ */
+public class TamsayiVeritipleri {
+
+    public static void main(String[] args) {
+      // int byte short long veri tipleri
+      // int veri tipi bellekte 4byte lık alan kaplar.
+      // 1 byte= 8 bit
+      int a = 4;
+      
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        
+        System.out.println("*");
+        
+        byte b = 100;
+        
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        
+        System.out.println("*");
+        
+        short c = 150;
+        
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        
+        System.out.println("*");
+        
+        long d = 200;
+        
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        
+        
+        
+        
+    }
+}
