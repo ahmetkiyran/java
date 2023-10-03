a725371c4ba5b94c6e1b6d77268d5c848a6dc3bc
ahmetkiyran <ahmet.kiyran1@gmail.com>
2023-10-03 17:19:31 +0300

    java

---
+++ Documents/NetBeansProjects/MatematikOperatorleri/src/main/java/com/mycompany/matematikoperatorleri/MatematikOperatorleri.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.matematikoperatorleri;
+
+/**
+ *
+ * @author Ahmet
+ */
+public class MatematikOperatorleri {
+
+    public static void main(String[] args) {
+       /* System.out.println(3 + 5.2);
+        System.out.println(3-5f);
+        System.out.println(10f/4);
+        System.out.println(3 * 4);
+        System.out.println(10 % 4);*/// kalanı bulduruyo mod yani.
+        
+        int a = 8;
+        
+        a+=2;
+        
+        System.out.println(a);
+        
+        int b = 3;
+        
+        b*=3;
+        
+        System.out.println(b);
+        
+      // int b = 9;
+      //  System.out.println(b++);
+      //  System.out.println(++b);
+      // b++ da güncellenmeden direkt 9 degerini verir ama
+      // ++b de 10 degerini ekrana bastırır.
+        
+    }
+}
