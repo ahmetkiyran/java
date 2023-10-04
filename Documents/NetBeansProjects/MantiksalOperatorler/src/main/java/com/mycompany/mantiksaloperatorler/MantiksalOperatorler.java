d142996f44124f8384f76247e35d28ea89aa35f2
ahmetkiyran <ahmet.kiyran1@gmail.com>
2023-10-04 19:27:51 +0300

    java

---
+++ Documents/NetBeansProjects/MantiksalOperatorler/src/main/java/com/mycompany/mantiksaloperatorler/MantiksalOperatorler.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mantiksaloperatorler;
+
+/**
+ *
+ * @author Ahmet
+ */
+public class MantiksalOperatorler {
+
+    public static void main(String[] args) {
+        
+        /*
+        && and operatörü
+        bütün sonuçlar true ise genel sonuç true, en az birisi bile false ise genel sonuç false olur.
+        
+        || or operatörü
+        sonuçlardan en az birisi true ise genel sonuç true, hepsi false ise genel sonuç false olur.
+        
+        ! not operatörü
+        değil sembolüyle aynı işlevi görür true ise false false ise true sonucunu verir
+        */
+        
+     /* System.out.println(3==3);
+        System.out.println(2<3);
+        System.out.println("ahmet" == "ahmet");
+        
+        System.out.println(3==3 && 2 < 3 && "ahmet" == "ahmet"); */
+     
+     
+        System.out.println(3==3);
+        System.out.println(2>3);
+        System.out.println("ahmet" == "ahmet");
+        
+        System.out.println(3==3 || 2 > 3 || "ahmet" == "ahmet"); 
+       
+       
+
+        
+        
+        
+    }
+}
