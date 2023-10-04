43e053bf543b9559cc343a6b1ccbdbfa1acd70e3
ahmetkiyran <ahmet.kiyran1@gmail.com>
2023-10-04 19:14:00 +0300

    java

---
+++ Documents/NetBeansProjects/KarsilastirmaOperatorleri/src/main/java/com/mycompany/karsilastirmaoperatorleri/KarsilastirmaOperatorleri.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.karsilastirmaoperatorleri;
+
+/**
+ *
+ * @author Ahmet
+ */
+public class KarsilastirmaOperatorleri {
+
+    public static void main(String[] args) {
+        
+        /*
+           Karşılaştırma operatörleri
+           
+        
+        ==  eşit mii ?
+        !=  eşit değil mi ?
+        >   büyük mü ?
+        <   küçük mü ?
+        >=  büyük veya eşit mi ?
+        <=  küçük veya eşit mi ?
+        */
+        
+        System.out.println(3 == 4);
+        System.out.println(3 != 4);
+        System.out.println(3 > 4);
+        
+        
+        
+        
+        
+        
+    }
+}
