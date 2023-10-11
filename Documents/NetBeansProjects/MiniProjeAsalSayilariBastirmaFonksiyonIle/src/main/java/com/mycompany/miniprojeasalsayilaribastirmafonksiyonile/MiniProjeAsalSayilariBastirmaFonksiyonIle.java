34dac5e23eb4570334d6d0ae0fd92f45b3689719
ahmetkiyran <ahmet.kiyran1@gmail.com>
2023-10-11 19:19:43 +0300

    java

---
+++ Documents/NetBeansProjects/MiniProjeAsalSayilariBastirmaFonksiyonIle/src/main/java/com/mycompany/miniprojeasalsayilaribastirmafonksiyonile/MiniProjeAsalSayilariBastirmaFonksiyonIle.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miniprojeasalsayilaribastirmafonksiyonile;
+
+/**
+ *
+ * @author Ahmet
+ */
+public class MiniProjeAsalSayilariBastirmaFonksiyonIle {
+public static boolean asalmi(int sayi)
+{
+    for(int i=2;i<sayi;i++)
+    {
+        if(sayi % i == 0)
+        {
+            return false;
+        }                      
+    }
+    return true;
+}
+    public static void main(String[] args) {
+        
+        
+        for(int i = 2;i<1000;i++)
+        {
+            if(asalmi(i)){
+               System.out.println(i);
+            }
+            
+        }
+                
+               
+    }
+}
