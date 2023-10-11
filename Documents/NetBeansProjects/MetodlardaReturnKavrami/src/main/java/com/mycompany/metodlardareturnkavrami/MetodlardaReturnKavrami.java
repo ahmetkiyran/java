1371962e88c9385ccdce398ea10b7ad4b2974b93
ahmetkiyran <ahmet.kiyran1@gmail.com>
2023-10-11 16:40:54 +0300

    java

---
+++ Documents/NetBeansProjects/MetodlardaReturnKavrami/src/main/java/com/mycompany/metodlardareturnkavrami/MetodlardaReturnKavrami.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.metodlardareturnkavrami;
+
+/**
+ *
+ * @author Ahmet
+ */
+public class MetodlardaReturnKavrami {
+ public static int topla(int a,int b,int c)
+ {
+     return a+b+c;
+ }
+    public static void main(String[] args) {
+        System.out.println("toplam sonucu "+topla(2,4,8));
+    }
+}
