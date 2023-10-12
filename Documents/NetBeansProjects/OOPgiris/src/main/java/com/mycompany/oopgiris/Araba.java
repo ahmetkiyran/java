2832e911305defe55f156d62bf989b8ec350e924
ahmetkiyran <ahmet.kiyran1@gmail.com>
2023-10-12 14:11:23 +0300

    java

---
+++ Documents/NetBeansProjects/OOPgiris/src/main/java/com/mycompany/oopgiris/Araba.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oopgiris;
+
+/**
+ *
+ * @author Ahmet
+ */
+public class Araba {
+    public String renk;
+    public int kapilar;
+    public int tekerlekler;
+    public String motor;
+    public String model;
+}
