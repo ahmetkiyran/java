80f6cf99bbba9b2d707b61565d057253fa7f269a
ahmetkiyran <ahmet.kiyran1@gmail.com>
2023-10-29 14:31:08 +0300

    java

---
+++ Documents/NetBeansProjects/Constructor-Part2/src/main/java/com/mycompany/constructor/part2/test.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.constructor.part2;
+
+/**
+ *
+ * @author Ahmet
+ */
+public class test {
+    public static void main(String[] args){
+         Account account1 = new Account();
+         
+         //System.out.println(account1.getEmail());
+        // System.out.println(account1.getBakiye());
+         
+        
+        /*account1.setHesapNo("12356");
+        account1.setBakiye(1000);
+        account1.setIsim("ahmet kıyran");
+        account1.setEmail("ahmet.kiyran1@gmail.com");
+        account1.setTelefonNo("3334225");
+        
+        System.out.println("bakiye: "+account1.getBakiye());
+        System.out.println("hesap numarası: "+account1.getHesapNo());
+        System.out.println("isim: "+account1.getIsim());
+        System.out.println("email: "+account1.getEmail());
+        System.out.println("telefon nummarası: "+account1.getTelefonNo());*/
+        
+        //Account account2 = new Account("3464375",1000.0,"Ahmet kıyran","ahmet.kiyran1@gmail.com","5676587568");
+        
+        
+        //account2.parrayatir(1000);
+        //account2.paracekme(600);
+        
+        Account account2 = new Account("Ahmet","example@gmail.com","98667568");
+        
+       //System.out.println(account2.getEmail());
+       // System.out.println(account2.getBakiye());
+       
+       account2.biilgigöster();
+        
+        
+    }
+}
